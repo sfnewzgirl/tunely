f62fe4fb04939c949838cab7f5457d11d461415b
sfnewzgirl <YOUR_GITHUB_EMAIL_ADDRESS>
2016-09-15 13:22:35 -0700

    finishes sprint 2

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -11,27 +11,25 @@ angular
   // the second argument is a function that defines the capacities
   // of the controller.
 
-function AlbumsIndexController () {
+AlbumsIndexController.$inject = ['$http'];
+
+function AlbumsIndexController ($http) {
   var vm = this;
   vm.newAlbum = {};
 
   vm.newAlbum = {
-      name: 'Viva Hate',
-      artistName: 'Morrissey'
-  };
+    name: 'Viva Hate',
+    artistName: 'Morrissey'
+  }
 
-  vm.albums = [
-    {
-      name: 'Coming Home',
-      artistName: 'Leon Bridges'
-    },
-    {
-      name: 'Are We There',
-      artistName: 'Sharon Van Etten'
-    },
-    {
-      name: 'The Queen is Dead',
-      artistName: 'The Smiths'
-    }
-  ];
+  vm.albums = [];
+
+  $http({
+    method: 'GET',
+    url: '/api/albums'
+    }).then(function successCallback(response) {
+      vm.albums = response.data;
+    }); function errorCallback(response) {
+      console.log('There was an error getting the data', response);
+    };
 }
